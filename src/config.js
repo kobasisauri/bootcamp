bee0838d732a111e6367beb48a0c1b4fb5441835
kobasisauri <kobasisauri@gmail.com>
2022-09-04 16:54:25 +0400

    feat: configs & useIsMobile hook

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  token: '146e587035e0d1429bbf4c7d7707da02',
+  mobileWidth: 700,
+};
+
+export default config;
---
+++ src/helpers/hooks/useIsMobile.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import config from 'config';
+
+export const useIsMobile = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= config.mobileWidth;
+};
